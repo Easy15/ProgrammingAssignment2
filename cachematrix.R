231719b2157c26c509082a56e50b73bddecb660a
Easy15 <niallodeasy@hotmail.com>
2017-09-26 15:05:11 +0200

    Commit 1
    Own input for  Data Science Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Here a function is constructed with which the inverse of a given matrix is 
+## stored  
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    mat_inv <- NULL
+    set <- function(y) { 
+            x <<- y
+            mat_inv <<- NULL
+        }
+    get <- function() x
+    setinv <- function(solve) mat_inv <<- solve(x)
+    getinv <- function() mat_inv
+    list(set = set, 
+         get = get, 
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function solves the matrix, whilst first checking whether an inverse 
+## stored in the cache can be used
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    mat_inv <- x$getinv
+    if(!is.null(mat_inv)) {
+        message("getting cached data")
+        return(mat_inv)
+    }
+    data <- x$get()
+    mat_inv <- solve(data,...)
+    x$setinv 
+    return(mat_inv)
 }
